be770f022cde76a16baf4d4f76d04d0c0cde58ae
Alixscript <77194352+0l1xscr1pt@users.noreply.github.com>
2022-12-24 10:09:44 +0000

    caesar decipherment v1

---
+++ caesar_decipherment.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+size_t ft_strlen(char *str) {
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return i;
+}
+
+char *decipherment(char *str, int gap) {
+	int i;
+	char *new_string;
+	size_t len;
+
+	i = 0;
+	len = ft_strlen(str);
+	new_string = malloc(sizeof(char) * (len + 1));
+	while (str[i]) {
+		new_string[i] = str[i];
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			new_string[i] = (new_string[i] - 'A' - gap + 26) % 26 + 'A';
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			new_string[i] = (new_string[i] - 'a' - gap + 26) % 26 + 'a';
+		else if (str[i] >= '0' && str[i] <= '9')
+			new_string[i] = (new_string[i] - '0' - gap + 10) % 10 + '0';
+		i++;
+	}
+}
